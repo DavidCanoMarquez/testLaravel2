f75faadfee4ed821a965b45ee6d4796cf400a0fe
David Cano Marquez <david.canomarquez@students.hevs.ch>
2018-03-07 15:28:28 +0100

    test3 ok

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -24,7 +24,10 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase
         $helloWorld = new HelloWorld($this->pdo);
 
         $this->assertEquals('Hello World', $helloWorld->hello());
-       
+       //ok, corrected
+
+
+
     }
 
     public function testHello()
